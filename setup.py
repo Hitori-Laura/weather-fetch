2b6f2d6f132122657cf4a8b3e6fbad567b7d506a
Hitori-Laura <hitori_laura_sorgente@proton.me>
2025-03-15 18:56:38 +0100

    fixed directory issue

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     install_requires=[
         "geocoder",
         "requests",
-        "colorama",
-        "os"
+        "colorama"
     ],
     entry_points={
         "console_scripts": [
